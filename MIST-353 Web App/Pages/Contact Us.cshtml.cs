ed9f0e524a1184717f997f8cdd60b4f344a3a376
dawalter123 <143112522+dawalter123@users.noreply.github.com>
2024-09-19 01:22:56 -0400

    This is another test. Praticing pushing the code.

---
+++ MIST-353 Web App/Pages/Contact Us.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIST_353_Web_App.Pages
+{
+    public class Contact_UsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ MIST-353 Web App/Pages/Testing page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MIST_353_Web_App.Pages
+{
+    public class Testing_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
